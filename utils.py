e68e8fd969fb6f6fc9952f144800c2ead3e19327
Meraj <meraccos@gmail.com>
2024-03-24 10:53:50 +0900

    Add imports and functions for tensorboard logging

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import math
+from datetime import datetime
 import torch
 from torch.utils.data import Dataset
+from torch.utils.tensorboard import SummaryWriter
 
 
 class CharDataset(Dataset):
@@ -9,13 +11,10 @@ class CharDataset(Dataset):
         self.seq_length = seq_length
 
     def __len__(self):
-        return len(self.data) - self.seq_length
+        return len(self.data) - self.seq_length + 1
 
     def __getitem__(self, index):
-        return (
-            self.data[index : index + self.seq_length],
-            self.data[index + 1 : index + self.seq_length + 1],
-        )
+        return self.data[index : index + self.seq_length]
         
 
 def generate_positional_encodings(max_len, d_model):
@@ -27,3 +26,7 @@ def generate_positional_encodings(max_len, d_model):
     pos_encodings[:, 0::2] = torch.sin(position * div_term)
     pos_encodings[:, 1::2] = torch.cos(position * div_term)
     return pos_encodings.unsqueeze(0)
+
+def get_writer(model_name, dataset):
+    now = datetime.now().strftime("%b%d_%H-%M")
+    return SummaryWriter(f'runs/{now}_{dataset[0]}_{model_name}')
