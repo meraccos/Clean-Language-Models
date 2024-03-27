36ffeac1b6e1fcdb6d110bc8c2d5d0e135138042
Meraj <meraccos@gmail.com>
2024-03-27 16:33:04 +0900

    Update LSTM model configuration and evaluation method

--- lstm.py
+++ lstm.py
@@ -13,13 +13,21 @@ class LSTM(BaseLanguageModel):
             batch_size=64,
             eval_freq=200,
             save_freq=500,
+            log_freq=100,
             block_size=200,
         )
         self.hidden_size = 128
-        self.num_layers = 2
+        self.num_layers = 1
         self.lr = 0.01
         self.emb_size = 5
         self.prepare_model(model_path)
+        
+        self.hparams = {"batch_size": self.batch_size,
+                         "block_size": self.block_size,
+                         "hidden_size": self.hidden_size,
+                         "num_layers": self.num_layers,
+                         "emb_size": self.emb_size,
+                         "lr": self.lr}
 
     def prepare_model(self, model_path=None):
         self.model = LSTM_Network(
@@ -38,10 +46,10 @@ class LSTM(BaseLanguageModel):
         x = x.type(torch.int)
         return x, hidden
 
-    def eval_single_batch(self, x, y):
-        output, (h, c) = self.model(x, None)
+    def eval_single_batch(self, seq):
+        output, _ = self.model(seq[:, :-1], None)
         output = output.view(-1, self.n_vocab)
-        y = y.view(-1)
+        y = seq[:, 1:].reshape(-1)
         loss = F.cross_entropy(output, y)
         return output, loss
 
